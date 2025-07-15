079910833f29c026a0790a7544275af120c49d25
gpropadm <contato@faimoveis.com.br>
2025-07-14 21:25:10 -0300

    Adiciona GitHub Actions para deploy automático

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
+ // Configurar DATABASE_URL a partir de DATABASE_URL_POSTGRES
+  if (!process.env.DATABASE_URL && process.env.DATABASE_URL_POSTGRES) {
+    process.env.DATABASE_URL = process.env.DATABASE_URL_POSTGRES;
+  }
+
 declare global {
   var prisma: PrismaClient | undefined
 }
