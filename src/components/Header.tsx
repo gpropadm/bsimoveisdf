eb6065d0ede1dc841c4835ba64ab4b204743420c
gpropadm <gpropadm@github.com>
2025-09-30 21:09:11 -0300

    Set header to always transparent

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   }, [])
 
   return (
-    <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${(isScrolled || isOnPageWithoutHero) ? 'bg-white' : 'bg-transparent'}`}>
+    <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 bg-transparent`}>
       <nav className={`container mx-auto px-4 transition-all duration-300 ${isScrolled ? 'py-6' : 'py-6'}`}>
         <div className="flex items-center justify-between">
           {/* Logo */}
